04e49450dd82ed6d0f0d77a35a3700ef98db66c5
gatodass <dass963@gmail.com>
2023-08-02 12:08:28 -0500

    Colas

--- i003-ms-notacredito/src/main/java/dpworld/com/ec/client/ActiveMQClient.java
+++ i003-ms-notacredito/src/main/java/dpworld/com/ec/client/ActiveMQClient.java
@@ -1,13 +1,19 @@
 package dpworld.com.ec.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ActiveMQClient {
+	private static final Logger logger = LoggerFactory.getLogger(ActiveMQClient.class);
 
 	@JmsListener(destination = "N4CREDITNOTES")
 	public void processMessage(String content) {
-		System.out.println(content);
+        logger.info("");
+        logger.info( this.getClass().getSimpleName());
+        logger.info("Received message of type: " + content.getClass().getSimpleName());
+        logger.info("Received message :" + content);
 	}
 }
---
+++ i003-ms-notascredito/src/main/java/dpworld/com/ec/I003MsNotascreditoApplication.java
@@ -0,0 +1,13 @@
+package dpworld.com.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I003MsNotascreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I003MsNotascreditoApplication.class, args);
+	}
+
+}
