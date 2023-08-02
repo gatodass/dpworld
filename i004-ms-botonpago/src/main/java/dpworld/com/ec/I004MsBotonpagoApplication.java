e0df64d72e95ea273d9d0b1e417a2b2583f67d8a
gatodass <dass963@gmail.com>
2023-08-01 23:53:15 -0500

    carga inicial

---
+++ i004-ms-botonpago/src/main/java/dpworld/com/ec/I004MsBotonpagoApplication.java
@@ -0,0 +1,13 @@
+package dpworld.com.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I004MsBotonpagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I004MsBotonpagoApplication.class, args);
+	}
+
+}
---
+++ i004-ms-botonpago/src/test/java/dpworld/com/ec/I004MsBotonpagoApplicationTests.java
@@ -0,0 +1,13 @@
+package dpworld.com.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I004MsBotonpagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
